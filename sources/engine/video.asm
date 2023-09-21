aa120a06cdafca23304df3ef6fe022f42c05d9ab
Louis Fourcade <louis.fourcade142@gmail.com>
2023-09-21 17:10:54 +0200

    video utils work

--- sources/engine/video.asm
+++ sources/engine/video.asm
@@ -20,86 +20,58 @@
 
 
 ;--------------------------------------------------------------------------
-;- wait_ly(b = ly à attendre)                                       -
+;- wait_ly(b = ly to wait for)                                       -
 ;--------------------------------------------------------------------------
-
 wait_ly::
-
-	ld		c,rLY & $FF
-
-.pas_le_bon_ly:
+	ld		c, rLY & $FF
+.not_yet:
 	ld		a,[$FF00+c]
 	cp		a,b
-	jr		nz,.pas_le_bon_ly
-
+	jr		nz,.not_yet
 	ret
 
-
-
 ;--------------------------------------------------------------------------
-;- wait_frames(e) = nb de frames à attendre)                                   -
+;- wait_frames(e = nb of frames to wait)                                   -
 ;--------------------------------------------------------------------------
-
 wait_frames::
-
     call    wait_vbl
     dec     e
     jr      nz,wait_frames
-
     ret
 
-
-
 ;--------------------------------------------------------------------------
-;- screen_off()                                                           -
+;- screen_off()     
+;- 		saves screen state and off                                                      -
 ;--------------------------------------------------------------------------
-
 screen_off::
-
     ld      a,[rLCDC]
-    ; sauvegarde des attributs
+    ; attributes save
     ld		[_screen_control_save], a
-
     and     a,LCDCF_ON
     ret     z ; LCD already OFF
-
-
     ld      b,$91
-    call    wait_ly
-
-
+    call    wait_ly ; wait for frame to draw
     xor     a,a
     ld      [rLCDC], a ;Shutdown LCD
-
     ret
 
-
-
 ;--------------------------------------------------------------------------
-;- screen_restart()                                                           -
+;- screen_restart()       
+;-		restores screen state and on                                                    -
 ;--------------------------------------------------------------------------
-
 screen_restart::
-
 	ld		a, [_screen_control_save]
-	or		a, LCDCF_ON ; pour être sûr de bien redémarrer l'écran
-
-
+	or		a, LCDCF_ON ; to be sure screen will start
 	ld		[rLCDC], a ; reprise de l'écran
-
-
 	ret
 
 	
 
-
-
 ;--------------------------------------------------------------------------
-;- init_DMA()                                        -
+;- init_DMA()    
+;-		Init DMA routine in HRAM                                    -
 ;--------------------------------------------------------------------------
-
 init_DMA::
-	
 	ld		b,__DMA_code_end - __DMA_code
 	ld		hl,__DMA_code
 	ld		de,DMA_ROUTINE_HRAM
@@ -108,27 +80,20 @@ init_DMA::
 
 
 
-	
-
-
-
 ;--------------------------------------------------------------------------
-;- call_DMA()                                        -
+;- call_DMA()  
+;-		Call DMA routine in HRAM                                      -
 ;--------------------------------------------------------------------------
-
-
 call_DMA::
-	ld		a,OAM_mirror >> 8 ;décalé de 8 bits car on ne prends que l'octet le plus significatif sur les deux composant l'adresse
+	ld		a,OAM_mirror >> 8 ; 8 bit offset becaus only High part of adress matters
 	jp		DMA_ROUTINE_HRAM
 
 
 
-
 ;--------------------------------------------------------------------------
-;- vram_set()   d = set value	 bc = size      hl = dest address   -
-;- effectue une copie vers la vram (attent de pouvoir y accéder)
+;- vram_set(d = set value ; bc = size ; hl = dest address)
+;- copy value d in VRAM
 ;--------------------------------------------------------------------------
-
 vram_set::
 	ld 		a, [rSTAT]
 	bit 	1, a
@@ -143,7 +108,7 @@ vram_set::
 	ret
 
 ;--------------------------------------------------------------------
-;- vram_set_inc()	d = start value 	bc = size 	hl = dest adress
+;- vram_set_inc(d = start value; bc = size ; hl = dest adress)
 ; @ensures : set vram values to an incrementing value
 ; [hl] -> |d|d+1|d+2|d+3|d+4|...etc...
 ;--------------------------------------------------------------------
@@ -162,41 +127,35 @@ vram_set_inc::
 	ret
 
 ;--------------------------------------------------------------------------
-;- vram_copy()    bc = size    hl = source address    de = dest address   -
-;- effectue une copie vers la vram (attent de pouvoir y accéder)
+;- vram_copy(bc = size ; hl = source address ; de = dest address)
 ;--------------------------------------------------------------------------
-
 vram_copy::
+    ld      a, [rSTAT]
+    bit     1, a
+    jr      nz, vram_copy ; Not mode 0 or 1
 
-    ld      a,[rSTAT]
-    bit     1,a
-    jr      nz,vram_copy ; Not mode 0 or 1
-
-    ldi      a,[hl]
-    ld      [de],a
+    ld      a, [hli]
+    ld      [de], a
     inc     de
     dec     bc
-    ld      a,b
-    or      a,c
-    jr      nz,vram_copy
+    ld      a, b
+    or      a, c
+    jr      nz, vram_copy
     ret
 
 ;--------------------------------------------------------------------------
-;- vram_copy_fast()    c = size    hl = source address    de = dest address   -
-;- effectue une copie vers la vram (attent de pouvoir y accéder)
+;- vram_copy_fast(c = size ; hl = source address ; de = dest address)
 ;--------------------------------------------------------------------------
-
 vram_copy_fast::
+    ld      a, [rSTAT]
+    bit     1, a
+    jr      nz, vram_copy_fast ; Not mode 0 or 1
 
-    ld      a,[rSTAT]
-    bit     1,a
-    jr      nz,vram_copy_fast ; Not mode 0 or 1
-
-    ldi      a,[hl]
-    ld      [de],a
+    ld      a,[hli]
+    ld      [de], a
     inc     de
     dec     c
-    jr      nz,vram_copy_fast
+    jr      nz, vram_copy_fast
     ret
 
 ;--------------------------------------------------------------------------
@@ -204,7 +163,6 @@ vram_copy_fast::
 ;- effectue une copie vers la tilemap du background (attend de pouvoir y accéder)
 ;- copie b*c bytes (tile number) en mettant en place les bonnes dimensions
 ;--------------------------------------------------------------------------
-
 tilemap_bg_block_copy::
 
 	push 	bc
@@ -245,7 +203,6 @@ tilemap_bg_block_copy::
 ;- effectue une copie vers la tilemap de la window (attend de pouvoir y accéder)
 ;- copie b*c bytes (tile number) en mettant en place les bonnes dimensions
 ;--------------------------------------------------------------------------
-
 tilemap_win_block_copy::
 	push 	bc
 	ld 		a, d
@@ -265,10 +222,7 @@ tilemap_win_block_copy::
 ;| |                          VARIABLES                                      | |
 ;| +-------------------------------------------------------------------------+ |
 ;+-----------------------------------------------------------------------------+
-
-
 	SECTION "Video_Variables",WRAM0[$C000]
-
 
 OAM_mirror:	DS $A0 ; copiée en OAM par la routine DMA
 OAM_mirror_end:
