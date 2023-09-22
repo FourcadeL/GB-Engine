7df1859b4e416995ca20cdce97a3718175f0e06e
Louis Fourcade <louis.fourcade142@gmail.com>
2023-09-22 19:34:00 +0200

    fwf text displayer automaton

---
+++ sources/engine/text/fwf_engine/fwf_text_displayer.asm
@@ -0,0 +1,12 @@
+; ########################
+; Fixed Width Font 
+; Text displayer AUTOMATON
+; #########################
+
+INCLUDE "hardware.inc"
+INCLUDE "charmap.inc"
+
+    SECTION "fwf_automaton_code",  ROM0
+
+
+    SECTION "fwf_automaton_variables", ROM0
