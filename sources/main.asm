ea28b8560096f9afedffee194ecabee37411f1dd
Louis Fourcade <louis.fourcade142@gmail.com>
2023-09-29 10:35:35 +0200

    debug room

--- sources/main.asm
+++ sources/main.asm
@@ -27,38 +27,9 @@ Main::
 
 	call 	Main_init
 	PRINT_DEBUG "Main Init Done"
-	ld c, $01
-	call fwf_init
-	PRINT_DEBUG "FWF Init Done"
+	call room_main
 
-	ld de, $9801
-	; ld hl, _test_text
-	ld hl, _text
-.loop
-	ld a, [hl]
-	cp a, "\n"
-	jr z, .change_text
-	push hl
-	ld l, a
-	push de
-	call fwf_display_char
-	ld e, 10
-	call wait_frames
-	pop de
-	inc de
-	pop hl
-	inc hl
-	nop
-	jr .loop
-.change_text
-	call fwf_flush
-	ld hl, $9800
-	ld d, $00
-	ld bc, $0400
-	call vram_set
-	ld de, $9801
-	ld hl, _test_text
-	jr .loop
+	
 
 
 ;---------------------------------------------
@@ -116,7 +87,9 @@ Main_init::
 	ret ; retour à l'exécution
 
 
-
+Main_vblk:
+	call call_DMA
+	ret
 
 
 
@@ -124,33 +97,9 @@ Main_init::
 	;-----------------------------
 	;    test data
 	;-----------------------------
-Main_vblk:
-	call call_DMA
-	ret
 
-	SECTION "Test_data", ROM0
+ 	SECTION "Placeholder_Tiles", ROM0
 
 _test_data_start:
 INCBIN "./engine/engine_data/test_tileset.bin"
 _test_data_end:
-
-
-_text:
-	DB "OK! Maintenant il est temps de s'ôter de tous les doutes sur les capacités de ce à quoi pourrait servir le moteur textuel. Ne pas hésiter sur les accents (voilà comme ça), les caractères spéciaux #ilespaspret, les signes en plus @$+ etc..."
-	DB "En plus de ça j'ajoute des MAJUSCULE pour GONFLER les TILES UTILISÉES : ça va faire mal ! Alors, on en est où maintenant de compteur ? ça a déjà beugé où bien ça marche de OUF ?\n"
-
-_test_text:
-	DB "Bonjour ceci est un test j'ajoute des tiles en plus MAJUSCULE"
-	DB "et minuscule pour voir jusqu'ou va tenir mon moteur textuel"
-	DB "J'ai l'impression qu'il faut que je commence à écrire beaucoup de merde"
-	DB "pour le pousser à bout et finalement dépasser les capacités prévues."
-	DB "Là les caractères spéciaux ne sont pas implémentés donc forcément il y a plein"
-	DB "de carrés moches et pas jolis"
-	DB "J'avoue ne pas avoir d'idée du nombre de caractères réutilisés dans la langue française ..."
-	DB "Peut être que là ça a été dépassé et qu'on ne voit plus que des \"carrés moches\" ?"
-	DB "Ou alors l'approche est méga robuste et je suis trop heureux !"
-	DB "OMG whaou ça marche VrAiMeNt SuPeR BiEn !"
-	DB "OBLIgé D'ECRiRe en majUSCUle pour Voir (LeS CAs LIMITES)"
-	DB "qmkjswcvxhoniUITD YREVDBFUYWXOI"
-	DB "ET MEME COMME ça je l'atteint PAS Qxlnwnds !"
-	DB "C'est OUUUUUUUUUUUF JE suis trop trop CONTENT de mon BOULOT !\n"
