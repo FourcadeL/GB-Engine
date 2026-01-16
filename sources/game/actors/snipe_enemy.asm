2ffa2dda887029241c1bf63e1ee07423a6641bd8
FourcadeL <louis.fourcade142@gmail.com>
2026-01-16 12:32:02 +0100

    WIP snipe_enemy

--- sources/game/actors/snipe_enemy.asm
+++ sources/game/actors/snipe_enemy.asm
@@ -1,6 +1,10 @@
 ; ################################
 ; Sniper enemy actor
 ;
+;       Sniper enemy will come from a start position,
+;       go to a specified target position, fire X shots,
+;       then go back to its start position and destroy itself
+;
 ;   handle functions
 ; ################################
 
@@ -60,45 +64,35 @@ Snip_enemy_init::
 
 
 ; ---------------------------------------
-; Snip_enemy_request(b = x pixel pos,
-;       c = entry flag and height target (%ehhhhhhh)
-;       d = shoot number,
-;       e = shot speed)
-;   Request a new sniper enemy at position b
-;   With height target before shooting c (h)
-;   Enemy will come from top if c (e) is reset and from botom if c (e) is set
+; Snip_enemy_request(b = X start position,
+;       c = Y start position
+;       Stackpush : starting and target positions
+;           $XTYT (target position 8 bits)
+;           $SSNN (S = enemy displacement speed | N = number of shots to fire)
+;           $RRSS (R = shoot rate | S = shot speed)
+;
+;   Request a new sniper enemy at position $XXYY
+;   With target $XTYT
 ; ---------------------------------------
 Snip_enemy_request::
-    push de
     push bc
     ACTOR_FIND_FREE                         ; find actor (hl, de are set)
     pop bc
-    jr z, .proceed          ; no actor found
-        ; no free slot
-        pop de
-        ret
-.proceed
+    ret nz                  ; no actor found
+
         ; add sniper enemy at hl and de
     ; sprite data
-    push bc
-    ld a, %10000001
+    ld a, %10000001                         ; active sprite
     ld [hl+], a
-    ld a, Snip_enemy_displayList_first_entry_index
-    ld b, $F8                               ; top entry Y
-    bit 7, c                                ; botom entry ?
-    jr z, .no_correction
-        ld b, BOTOM_ENTRY_Y_VALUE
-        add a, 2
-.no_correction
+    ld a, Snip_enemy_displayList_first_entry_index  ; default display list (changed at update)
     ld [hl+], a
-    swap b
-    ld a, b
+    swap c
+    ld a, c
     and a, %11110000
     ld [hl+], a                             ; set Y low
-    ld a, b
+    ld a, c
     and a, %00001111
     ld [hl+], a                             ; set Y high
-    pop bc
     swap b
     ld a, b
     and a, %11110000                        ; set X low
@@ -107,36 +101,57 @@ Snip_enemy_request::
     and a, %00001111                        ; set X high
     ld [hl+], a
 
-    ld a, LOW(Snip_enemy_handle)
+    ld a, LOW(Snip_enemy_handle)            ; set handle function
     ld [hl+], a
     ld [hl], HIGH(Snip_enemy_handle)
 
-    ; actor data
-    ld h, d
-    ld l, e
-    pop de
-    ld a, ENTRY_STATE
-    ld [hl+], a
+    ; actor data                                    ; COULD BE OPTIMIZED WITH A LOOP
+    ld hl, sp + 7                           ; stackargs botom ; state set up
+    ld a, MOOVE_STATE
+    ld [de], a
+    inc e
         ; flags set up
-        ld a, c
-        and a, %10000000
-        rlc a
-        ld [hl+], a
+    xor a
+    ld [de], a
+    inc e
+        ; init pos set up
+    swap b                                  ; x initial
+    ld a, b
+    ld [de], a
+    inc e
+    swap c                                  ; y initial
     ld a, c
-    and a, %01111111
-    ld [hl+], a                                 ; set y_target
-    ld a, d
-    ld [hl+], a                                 ; set shoot_nb
-    ld a, 98                                    ; TODO : shoot rate default
-    ld [hl+], a
-    ld a, e
-    ld [hl+], a                                 ; set shot_speed
+    ld [de], a
+    inc e
+        ; target set up
+    ld a, [hl-]                             ; x target
+    ld [de], a
+    inc e
+    ld a, [hl-]                             ; y target
+    ld [de], a
+    inc e
+        ; speed set up
+    ld a, [hl-]                             ; enemy speed
+    ld [de], a
+    inc e
+        ; shot set up
+    ld a, [hl-]                             ; shot nb
+    ld [de], a
+    inc e
+    ld a, [hl-]                             ; shoot rate
+    ld [de], a
+    inc e
+    ld a, [hl-]                             ; shot speed
+    ld [de], a
+    inc e
+
     ld a, ANIM_COUNTER_VALUE
-    ld [hl+], a                                 ; set animation counter
+    ld [de], a                                 ; set animation counter
+    inc e
 
         ; framerule set
-    ld de, snip_enemy_next_assign_framerule
-    ld a, [de]
+    ld hl, snip_enemy_next_assign_framerule
+    ld a, [hl]
     inc a
     and a, %00000011
     ld [de], a
@@ -157,10 +172,8 @@ Snip_enemy_handle:
 ;     ld h, d
 ;     ld l, a
     ld a, [de]                                  ; get current state (first byte)
-    cp a, ENTRY_STATE
-    jr z, entry_state_handle
-    cp a, LEAVE_STATE
-    jr z, leave_state_handle
+    cp a, MOOVE_STATE
+    jr z, moove_state_handle
     cp a, SHOOT_STATE
     jr z, shoot_state_handle
     cp a, DEAD_STATE
@@ -168,13 +181,184 @@ Snip_enemy_handle:
 
     ret
 
-entry_state_handle:
-leave_state_handle:
+moove_state_handle:
+    push de
+    push bc
+    ld h, b
+    ld a, SPRITE_STRUCT_Ypos
+    add a, c
+    ld l, a
+    ld a, [hl+]
+    and a, %11110000
+    ld c, a
+    ld a, [hl+]
+    and a, %00001111
+    or a, c
+    swap a
+    ld c, a                                     ; c <- enemy pixel Y pos
+
+    ld a, [hl+]
+    and a, %11110000
+    ld b, a
+    ld a, [hl+]
+    and a, %00001111
+    or a, b
+    swap a
+    ld b, a                                     ; b <- enemy pixel X pos
+
+    ld h, d
+    ld a, x_target
+    add a, e
+    ld l, a
+    ld a, [hl+]
+    ld d, a                                     ; d <- target pixel X pos
+
+    ld a, [hl+]
+    ld e, a                                     ; e <- target pixel Y pos
+
+    ld a, [hl]                                  ; a <- displacement speed
+
+    call Target_get_displacement_vector
+    jr z, .null_vector
+            ; bc and de are X Y displacement vectors
+    pop hl
+    push hl
+    ld a, SPRITE_STRUCT_Ypos
+    add a, l
+    ld l, a
+        ; add vector to sprite position
+    ld a, [hl]
+    add a, e
+    ld [hl+], a
+    ld a, [hl]
+    adc a, d
+    ld [hl+], a
+    ld a, [hl]
+    add a, c
+    ld [hl+], a
+    ld a, [hl]
+    adc a, b
+    ld [hl], a
+    
+    pop bc
+    pop de
+
+    jp collision_handle
+.null_vector
+    pop bc
+    pop de
+
+    ld a, flags
+    ld h, d
+    add a, e
+    ld l, a
+    bit 7, [hl]
+    jr z, .set_next_state
+        ; already on its way back, delete
+        xor a
+        ld [bc], a                              ; reset sprite flags
+        ret
+.set_next_state
+    set 7, [hl]                                 ; set return flags
+        ; set shoot state
+    ld a, SHOOT_STATE
+    ld [de], a
+        ; copy new target pixel pos
+    ld b, d
+    ld a, x_initial
+    add a, e
+    ld c, a
+    ld a, x_target
+    add a, e
+    ld l, a
+    ld a, [bc]
+    inc c
+    ld [hl+], a
+    ld a, [bc]
+    ld [hl], a
+
+    ret
+        
+    
+
+
 shoot_state_handle:
+    ; TODO
+    jr collision_handle
+
 dead_state_handle:
+        ; delete sprite
+    ld a, 0
+    ld [bc], a
+        ; add explosion at former position
+    ld a, SPRITE_STRUCT_Ypos
+    add a, c
+    ld l, a
+    ld h, b
+    ld a, [hl+]
+    and a, %11110000
+    ld c, a
+    ld a, [hl+]
+    and a, %00001111
+    or a, c
+    swap a
+    ld c, a                         ; c <- y pixel pos
+    ld a, [hl+]
+    and a, %11110000
+    ld b, a
+    ld a, [hl]
+    and a, %00001111
+    or a, b
+    swap a
+    ld b, a
+    jp Explosion_request
+
+
+;-------------------------
+; collision_handle(bc = sprite addr, de = actor data addr)
+;
+;   Tests against all enemy shots if there is a collision
+;   Test player collision and set player flag if collision occured
+;   Test is done only on current enemy framerule
+;------------------------
+collision_handle:
+        ; check only on framerule
+    ld a, e
+    add a, framerule
+    ld l, a
+    ld h, d
+    ld a, [Global_counter]
+    and a, %00000011
+    sub a, [hl]
+    ret nz                          ; don't update on wrong framerule
+        ; handle collision with player shot
+    ; (assume that bc and de are still set)
+    push de
+    ACTOR_PLAYER_SHOT_COLLISION_SQUARE 9, 10, .shot_collision, .check_player_collision
+.shot_collision
+    pop hl
+    ld a, state
+    add a, l
+    ld l, a
+    ld [hl], DEAD_STATE
     ret
 
-
+    ; check player collision (b = enemy Y pixel pos; c = enemy X pixel pos)
+.check_player_collision
+    ACTOR_PLAYER_COLLISION_SQUARE c, b, SNIP_E_HITBOX_WIDTH, SNIP_E_HITBOX_HEIGHT, .no_player_collision
+        ; set collision flag for player
+    ld hl, player_state
+    set 5, [hl]
+        ; set enely in dead state
+    pop hl
+;     ld a, state
+;     add a, l
+;     ld l, a                       ; state is the first byte of structure
+    ld [hl], DEAD_STATE
+    ret
+.no_player_collision
+    pop de
+    ret
 
     SECTION "Snip_enemy_display_lists", ROMX, ALIGN[4]
 snip_enemy_dl_frame1:
